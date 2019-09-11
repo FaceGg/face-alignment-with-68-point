e3651539702791b38d35c1218d61e37ca16bd669
FaceGg <44217462+FaceGg@users.noreply.github.com>
2019-09-11 15:09:59 +0800

    Update 1_train.sh

--- train_300w_68_vanilla/1_train.sh
+++ train_300w_68_vanilla/1_train.sh
@@ -3,11 +3,11 @@
 
 set -e
 
-TOOLS=/home/jms_test/caffe-master-python2/.build_release/tools/
+TOOLS=/home/test/caffe-master-python2/.build_release/tools/
 
 
 $TOOLS/caffe train \
--solver /home/jms_test/6/face_alignment-master/train_300w_68_vanilla/1_solver.prototxt \
+-solver /home/test/6/face_alignment-master/train_300w_68_vanilla/1_solver.prototxt \
 -gpu 0 \
 -snapshot model1/1_solver_iter_4500000.solverstate
 
